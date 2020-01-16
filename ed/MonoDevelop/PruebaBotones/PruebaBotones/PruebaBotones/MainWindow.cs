b867563a07428a59f723a870138a24456f61aa5e
jean180 <jean_2_0_0_0_@hotmail.com>
2020-01-16 08:58:43 +0100

    Introduccion entorno grafico

---
+++ ed/MonoDevelop/PruebaBotones/PruebaBotones/PruebaBotones/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnSaludarClicked(object sender, EventArgs e)
+    {
+        MessageDialog md = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Hola");
+        md.Run();
+        md.Destroy();
+    }
+}
