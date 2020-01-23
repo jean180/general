869299993dd7597a282757dc8ad70c12b5164d10
jean180 <jean_2_0_0_0_@hotmail.com>
2020-01-23 08:55:23 +0100

    Prueba bingo

---
+++ Prg/Eclipse/src/Bingo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author jeaagu
+ *
+ */
+public class Bingo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		//Bienvenida
+		System.out.println("Hola, bienvenido al Bingo Bongo, elige un número para asignarte un carton");
+		int carton = teclado.nextInt();
+	}
+
+}
