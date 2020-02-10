e3397745a89ca581ac4417c8da349eba14b101c4
jean180 <jean_2_0_0_0_@hotmail.com>
2020-02-10 13:59:27 +0100

    Introduccion numeros

---
+++ ed/MonoDevelop/CCalculadora/CCalculadora/CCalculadora/MainWindow.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    int contador;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBVaciarClicked(object sender, EventArgs e)
+    {
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+    }
+
+    protected void OnBBorrarClicked(object sender, EventArgs e)
+    {
+        Pantalla.DeleteText(Pantalla.Text.Length - 1, Pantalla.Text.Length);
+    }
+
+    protected void OnBIgualClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBSumarClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBDividirClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBMultiplicarClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBRestarClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBPuntoClicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        if (Display.Contains(".") == false)
+        {
+            if (Display.Equals(""))
+            {
+                Pantalla.DeleteText(0, Pantalla.Text.Length);
+                Pantalla.InsertText("0.");
+            }
+            else
+            {
+                Pantalla.DeleteText(0, Pantalla.Text.Length);
+                Pantalla.InsertText(Display + ".");
+            }
+           
+        }
+    }
+
+    protected void OnBCeroClicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "0");
+    }
+
+    protected void OnB1Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "1");
+    }
+
+    protected void OnB2Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "2");
+    }
+
+    protected void OnB3Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "3");
+    }
+
+    protected void OnB4Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "4");
+    }
+
+    protected void OnB5Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "5");
+    }
+
+    protected void OnB6Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "6");
+    }
+
+    protected void OnB7Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "7");
+    }
+
+    protected void OnB8Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "8");
+    }
+
+    protected void OnB9Clicked(object sender, EventArgs e)
+    {
+        String Display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(Display + "9");
+    }
+
+}
